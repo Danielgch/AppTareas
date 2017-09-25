e22417b7834fb39138fe3ec567adccc5a9302dcd
Daniel <danielgiraldoch@gmail.com>
2017-09-25 16:04:08 -0500

    segundo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  title: string = "Angular Crud";
+  employees = [{name: 'Daniel', position: 'manager', email: 'danielgiraldoch@gmail.com'},
+  {name: 'Juan', position: 'Designer' , email: 'camilogrialdo@gmail.com'},
+  {name: 'Carlos', position: 'Development', email: 'dasdasdasd@gmail.com'}]
+  model:any ={}; 
+
+  addEmployee():void{
+    this.employees.push(this.model);
+  }
+  deleteEmployee():void{
+
+  }
+  editEmployee():void{
+
+  }
 }
